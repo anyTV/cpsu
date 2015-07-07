8c9c0a242cee32f77559dd593b530335b1774614
ronaldpabilonia <ron@any.tv>
2015-07-07 18:58:57 +0800

    fix updater

--- app.js
+++ app.js
@@ -2,56 +2,53 @@ var app = require('http').createServer(handler),
     logger = require('anytv-node-logger'),
     cudl = require('cuddle')(logger),
     io = require('socket.io')(app),
+    _ = require('lodash'),
     fs = require('fs'),
-    streamers,
+    streamers = [],
     recheck,
     timer;
 
 app.listen(9999);
 
-function find_streamers (data) {
-  cudl.get
-    .to('http://www.you1tube.com:9999/find_streamers')
-    .send(data)
-    .then(function (err, result) {
-      if (io.engine.clients) {
-        if (err) {
-          io.emit('online_streamers', err);
-        }
-
-        io.emit('online_streamers', result);
-      }
-    });
-}
-
-function start_check (data) {
-  clearInterval(timer);
-  find_streamers(data);
-  setTimeout(start_check, 900000);
-}
-start_check();
-
 function handler (req, res) {
-    fs.readFile(__dirname + '/index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
+  fs.readFile(__dirname + '/index.html',
+    function (err, data) {
+      if (err) {
+          res.writeHead(500);
+          return res.end('Error loading index.html');
+      }
 
-            res.writeHead(200);
-            res.end(data);
-        });
+      res.writeHead(200);
+      res.end(data);
+    });
 }
 
 io.on('connection', function (socket) {
   socket.on('user_favorite_streamers', function (data) {
-    streamers = data;
+    streamers.push(data);
   });
 });
 
-timer = setInterval(function () {
-  if (streamers) {
-    start_check(streamers);
-  }
-}, 1000);
+function start_check () {
+  streamers.length ? start_finding() : setTimeout(start_check, 1000);
+}
+start_check();
+
+function start_finding () {
+  _(streamers)
+    .forEach(function (a, key){
+      cudl.get
+        .to('http://www.you1tube.com/find_streamers')
+        .send(a)
+        .then(function (err, result) {
+          if (io.engine.clients) {
+            if (err) {
+              io.emit('online_streamers', err);
+            }
+
+            io.emit('online_streamers', result);
+          }
+        });
+    }).
+    commit();
+}
