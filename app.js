29670bb986950f7ff72f372fd8d759c08b343b69
ronaldpabilonia <ron@any.tv>
2015-07-20 16:09:24 +0800

    fixed spacing

--- app.js
+++ app.js
@@ -10,46 +10,47 @@ var app = require('http').createServer(handler),
 
 app.listen(9999);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-          res.writeHead(500);
-          return res.end('Error loading index.html');
-      }
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
 
-      res.writeHead(200);
-      res.end(data);
-    });
+            res.writeHead(200);
+            res.end(data);
+        });
 }
 
 io.on('connection', function (socket) {
-  socket.on('user_favorite_streamers', function (data) {
-    streamers.push(data);
-  });
+    socket.on('user_favorite_streamers', function (data) {
+        streamers.push(data);
+    });
 });
 
-function start_check () {
-  streamers.length ? start_finding() : setTimeout(start_check, 1000);
+function start_check() {
+    streamers.length ? start_finding() : setTimeout(start_check, 1000);
 }
 start_check();
 
-function start_finding () {
-  _(streamers)
-    .forEach(function (a, key){
-      cudl.get
-        .to('http://www.you1tube.com/find_streamers')
-        .send(a)
-        .then(function (err, result) {
-          if (io.engine.clients) {
-            if (err) {
-              io.emit('online_streamers', err);
-            }
+function start_finding() {
+    _(streamers)
+        .forEach(function (a, key) {
+            cudl.get
+                .to('http://www.you1tube.com/find_streamers')
+                .send(a)
+                .then(function (err, result) {
+                    if (io.engine.clients) {
+                        if (err) {
+                            io.emit('online_streamers', err);
+                        }
 
-            io.emit('online_streamers', result);
-          }
-        });
-    }).
+                        io.emit('online_streamers', result);
+                    }
+                });
+        }).
     commit();
     setTimeout(start_finding, 900000);
 }
+
